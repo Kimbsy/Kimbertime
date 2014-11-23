6d8108d3e58ff8108e3b6118343391e18829d299
Kimbsy <lordkimber@gmail.com>
2014-11-23 17:09:07 +0000

    menu music works, game music doesnt, might just use menu music :)

--- Drive/Sound.java
+++ Drive/Sound.java
@@ -2,7 +2,7 @@ import java.applet.Applet;
 import java.applet.AudioClip;
 
 public class Sound {
-    public static final AudioClip MENU = Applet.newAudioClip(Sound.class.getResource("sound/back.wav"));
-    // public static final AudioClip BALL = Applet.newAudioClip(Sound.class.getResource("ball.wav"));
+    public static final AudioClip MENU = Applet.newAudioClip(Sound.class.getResource("sound/elevatorMusic.wav"));
+    // public static final AudioClip GAME = Applet.newAudioClip(Sound.class.getResource("sound/theMetal.wav"));
     // public static final AudioClip GAMEOVER = Applet.newAudioClip(Sound.class.getResource("gameover.wav"));
 }
