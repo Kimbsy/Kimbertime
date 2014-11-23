2ce1231162f669ecca5b46c8ac90a9f8b82265b9
Kimbsy <lordkimber@gmail.com>
2014-11-23 16:39:16 +0000

    cleaning up, sound working

---
+++ Drive/Sound.java
@@ -0,0 +1,8 @@
+import java.applet.Applet;
+import java.applet.AudioClip;
+
+public class Sound {
+    public static final AudioClip MENU = Applet.newAudioClip(Sound.class.getResource("sound/back.wav"));
+    // public static final AudioClip BALL = Applet.newAudioClip(Sound.class.getResource("ball.wav"));
+    // public static final AudioClip GAMEOVER = Applet.newAudioClip(Sound.class.getResource("gameover.wav"));
+}
